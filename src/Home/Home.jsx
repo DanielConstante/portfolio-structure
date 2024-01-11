2142e767aca217989dbcf3def60a88c269e03f75
Daniel Constante <dcons11@wgu.edu>
2024-01-11 14:36:49 -0600

    React Router Dom set up

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,8 +1,15 @@
+import React from "react";
+import { Button} from "@mui/material";
+import { Link } from "react-router-dom";
+
 function Home() {
-      return (
-            <>
-            <h1>Home Page</h1>
-            </>
-          )
-        } 
+  return (
+    <>
+      <h1>Home Page</h1>
+      <Button component={Link} to="/projects">
+        Click Me
+      </Button>
+    </>
+  )
+}
 export default Home;
